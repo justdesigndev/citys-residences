c7d3389587eb6bad4f98ead9edccc994c1ec505b
armagansenol <armagansnl@gmail.com>
2025-04-15 17:33:58 +0300

    feat: Enhance residences page with new video and stacking cards components
    - Added a video section with background video and text reveal animation for improved engagement.
    - Introduced StackingCards component to display residence options with images and click-to-view functionality.
    - Updated layout to include additional video content and improved visual structure.

--- app/[locale]/residences/page.tsx
+++ app/[locale]/residences/page.tsx
@@ -1,9 +1,91 @@
+import { cn } from "@/lib/utils"
+
 import { Wrapper } from "@/components/wrapper"
+import { Video } from "@/components/utility/video"
+import { mainVideoDesktop, melihBulgurVideo, mustafaTonerVideo } from "@/lib/constants"
+import { ScaleOut } from "@/components/animations/scale-out"
+import { VideoWithPlayButton } from "@/components/utility/video-with-play-button"
+import { StackingCards } from "@/components/stacking-cards"
+import { TextRevealOnScroll } from "@/components/animations/text-reveal-on-scroll"
 
 export default function Page() {
+  const items = [
+    {
+      title: "1+1",
+      images: [
+        {
+          url: "/img/residences/1+1/plan.jpg",
+        },
+        {
+          url: "/img/residences/1+1/interior.jpg",
+        },
+      ],
+    },
+    {
+      title: "2+1",
+      images: [
+        {
+          url: "/img/residences/2+1/plan.jpg",
+        },
+        {
+          url: "/img/residences/2+1/interior.jpg",
+        },
+      ],
+    },
+    {
+      title: "3+1",
+      images: [
+        {
+          url: "/img/residences/3+1/plan.jpg",
+        },
+        {
+          url: "/img/residences/3+1/interior.jpg",
+        },
+      ],
+    },
+  ]
+
   return (
     <Wrapper>
-      <section>Residences</section>
+      <section className={cn("h-[var(--svh-calc)] bt:h-screen bg-bricky-brick relative z-10 overflow-hidden")}>
+        <ScaleOut>
+          <div className="h-full w-full">
+            <Video
+              primaryVideoUrl={mainVideoDesktop}
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="absolute top-0 left-0 w-full h-full bg-black/50">
+            <div className="container flex h-full">
+              <h1 className="block font-lexend-giga text-white text-2xl bt:text-4xl font-medium mt-auto mb-20">
+                <TextRevealOnScroll splitBy="characters" textAlign="left" staggerDuration={0.005}>
+                  DAİRELER
+                </TextRevealOnScroll>
+              </h1>
+            </div>
+          </div>
+        </ScaleOut>
+      </section>
+      <section className="relative h-screen bg-white z-20 flex items-center justify-center">
+        <span className="text-8xl font-black text-center">3D</span>
+      </section>
+      <section>
+        <StackingCards items={items} />
+      </section>
+      <section className="relative container py-20">
+        <div className="w-full h-[350px] bt:h-auto bt:aspect-video overflow-hidden relative z-10 flex items-center justify-center bg-black rounded-md">
+          <VideoWithPlayButton primaryVideoUrl={mustafaTonerVideo} thumbnail="/img/thumbnail-toners.jpg" />
+        </div>
+      </section>
+      <section className="relative container py-20">
+        <div className="w-full h-[350px] bt:h-auto bt:aspect-video overflow-hidden relative z-10 flex items-center justify-center bg-black rounded-md">
+          <VideoWithPlayButton primaryVideoUrl={melihBulgurVideo} thumbnail="/img/thumbnail-melih-bulgur.jpg" />
+        </div>
+      </section>
     </Wrapper>
   )
 }
---
+++ components/stacking-cards/index.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { useGSAP } from "@gsap/react"
+import { useRef } from "react"
+
+import { ScrollTrigger, gsap } from "@/components/gsap"
+import { Img } from "@/components/utility/img"
+import { useImageGalleryStore } from "@/lib/store/image-gallery"
+
+export interface StackingCardsProps {
+  items: {
+    title: string
+    images: {
+      url: string
+    }[]
+  }[]
+}
+
+export function StackingCards({ items }: StackingCardsProps) {
+  const ref = useRef(null)
+  const { openModal } = useImageGalleryStore()
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline()
+      const cards: HTMLElement[] = gsap.utils.toArray(".gsap-stacking-card")
+
+      cards.forEach((card, index) => {
+        gsap.set(card, { yPercent: card.clientHeight * index * -1 })
+        tl.fromTo(card, { yPercent: () => (index === 0 ? 0 : 150) }, { yPercent: 15 * index })
+        // .to(card, { opacity: 0 }, `s_${index}`)
+      })
+
+      ScrollTrigger.create({
+        animation: tl,
+        trigger: ".gsap-stacking-cards-container",
+        start: "top top+=15%",
+        pin: true,
+        scrub: true,
+        end: "+=1500px",
+      })
+    },
+    {
+      scope: ref,
+    }
+  )
+
+  const handleImageClick = (itemImages: { url: string }[], index: number) => {
+    const slides = itemImages.map((image) => (
+      <div key={image.url} className="h-[60vh] w-[100vw] relative">
+        <Img src={image.url} fill sizes="100vw" alt="Residence Interior" className="object-contain" />
+      </div>
+    ))
+    openModal(slides, index)
+  }
+
+  return (
+    <div ref={ref}>
+      <div className="gsap-stacking-cards-container relative w-full h-[65vh] bt:h-[40vh] bd:h-[70vh] mb-64">
+        {items.map((item, i) => (
+          <div
+            className={cn(
+              "gsap-stacking-card absolute left-0 w-full h-full overflow-hidden border-t bg-white p-3 bt:p-10 flex items-end justify-end"
+            )}
+            key={i}
+          >
+            <h2 className="absolute top-0 left-0 font-halenoir text-3xl bt:text-5xl font-normal p-3 bt:p-5 tracking-widest">
+              {item.title}
+            </h2>
+            <div className="flex flex-col bt:flex-row gap-3 bt:gap-5 ml-auto w-8/12 bt:w-auto h-[100%] bt:h-[60%] bd:h-[85%]">
+              {item.images.map((image, i) => (
+                <div
+                  key={i}
+                  className="flex-shrink-0 bt:h-full rounded-md overflow-hidden cursor-pointer hover:opacity-90 transition-opacity"
+                  onClick={() => handleImageClick(item.images, i)}
+                >
+                  <Img src={image.url} alt={item.title} width={1000} height={1000} />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
