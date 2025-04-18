84cfb802ff1b4bcd82c029d8935da4ebeff5e437
armagansenol <armagansnl@gmail.com>
2025-04-18 16:52:17 +0300

    refactor: Revamp ModalContactForm component for improved functionality and styling
    - Replaced the previous props-based structure with a local state for managing the modal's open state.
    - Updated GSAP animations for smoother transitions and integrated Lenis for scroll management.
    - Refactored CSS styles to enhance layout and responsiveness, including adjustments to the modal's background and form presentation.
    - Cleaned up unused code and improved overall readability of the component.

--- components/modal-contact-form/index.tsx
+++ components/modal-contact-form/index.tsx
@@ -2,28 +2,27 @@
 
 import s from "./modal-contact-form.module.css"
 
+import { cn } from "@/lib/utils"
 import { useGSAP } from "@gsap/react"
-import cn from "clsx"
 import { X } from "lucide-react"
 import { useTranslations } from "next-intl"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 import { ContactForm } from "@/components/form-contact"
 import { gsap } from "@/components/gsap"
 import { ScrollableBox } from "@/components/utility/scrollable-box"
-import { Video } from "@/components/utility/video"
-import { FormTranslations } from "@/types"
 import { useEsc } from "@/hooks/useEsc"
-import { mainVideoDesktop } from "@/lib/constants"
+import { FormTranslations } from "@/types"
+import { useLenis } from "lenis/react"
 
-interface ModalContactFormProps {
-  open: boolean
-  setOpen: (open: boolean) => void
-}
+export function ModalContactForm() {
+  const [open, setOpen] = useState(false)
 
-export function ModalContactForm({ open, setOpen }: ModalContactFormProps) {
-  const ref = useRef<HTMLDivElement>(null)
+  const bgRef = useRef<HTMLDivElement>(null)
+  const formRef = useRef<HTMLDivElement>(null)
   const t = useTranslations("contact")
+  const commonT = useTranslations("common")
+  const lenis = useLenis()
 
   const formTranslations: FormTranslations = {
     inputs: {
@@ -117,66 +116,101 @@ export function ModalContactForm({ open, setOpen }: ModalContactFormProps) {
         paused: true,
       })
 
-      menuTL.current?.fromTo(
-        ref.current,
-        { clipPath: "inset(0% 0% 0% 100%)" },
-        {
-          clipPath: "inset(0% 0% 0% 0%)",
-          duration: 1.2,
-          ease: "expo.inOut",
-        }
-      )
+      menuTL.current
+        ?.fromTo(
+          bgRef.current,
+          { opacity: 0 },
+          {
+            opacity: 1,
+            duration: 0.6,
+            ease: "power2.inOut",
+          },
+          "s"
+        )
+        .fromTo(
+          formRef.current,
+          { width: "0" },
+          {
+            width: "700px",
+            duration: 1,
+            ease: "expo.inOut",
+          },
+          "s"
+        )
     },
 
     {
-      scope: ref,
       revertOnUpdate: true,
     }
   )
 
   useGSAP(
     () => {
-      return open ? menuTL.current?.play() : menuTL.current?.reverse()
+      if (open) {
+        menuTL.current?.play()
+        lenis?.stop()
+      } else {
+        menuTL.current?.reverse()
+        lenis?.start()
+      }
     },
+
     {
-      dependencies: [open],
+      dependencies: [open, lenis],
       revertOnUpdate: true,
     }
   )
 
   return (
-    <div className={cn(s.frame, "blur-bg")} ref={ref} onClick={() => setOpen(false)}>
-      <div className={cn(s.wrapper, "wrapper")}>
-        <div className={cn(s.menu, "menu")}>
-          <div className={cn(s.content, "flex items-center justify-center w-full h-full")}>
-            <div className={cn(s.box, "relative rounded-none bt:rounded-lg overflow-hidden h-full")}>
-              <button
-                className={cn(
-                  s.close,
-                  "absolute top-2 bt:top-4 right-2 bt:right-4 w-10 h-10 z-10 bg-white rounded-full p-2 text-bricky-brick"
-                )}
-                onClick={() => setOpen(false)}
-                type="button"
-              >
-                <X />
-              </button>
-              <div
-                className="bg-white flex flex-col bd:grid grid-cols-2 w-full h-full"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <div className="bd:col-span-1 flex h-full overflow-hidden">
-                  <ScrollableBox>
-                    <div className="px-4 bt:px-12 py-14 bt:py-16 space-y-8">
-                      <h2 className="text-neutral-900 text-base bt:text-sm font-normal font-halenoir text-left bt:text-center bd:text-left leading-normal">
-                        {t.rich("description", {
-                          br: () => <br className="hidden bt:block" />,
-                        })}
-                      </h2>
-                      <ContactForm translations={formTranslations} />
-                    </div>
-                  </ScrollableBox>
-                </div>
-                <div className="hidden bd:block col-span-1">
+    <>
+      <div
+        className={cn("fixed top-0 left-0 w-full h-full blur-bg opacity-0 z-[150]", {
+          "pointer-events-none": !open,
+        })}
+        ref={bgRef}
+        onClick={() => setOpen(false)}
+      ></div>
+      <div className={cn(s.form)}>
+        <div className={cn("relative box bg-white h-full w-0")} onClick={(e) => e.stopPropagation()} ref={formRef}>
+          <div className="absolute top-0 left-0 w-[700px] h-full flex right-0">
+            <button
+              className={cn(
+                s.close,
+                "absolute top-2 bt:top-6 -left-2 bt:-left-6 w-10 h-10 z-10 -translate-x-full bg-white rounded-full p-2 text-bricky-brick",
+                "opacity-0 transition-opacity duration-700 ease-in-out",
+                {
+                  "opacity-100": open,
+                }
+              )}
+              onClick={() => setOpen(false)}
+              type="button"
+            >
+              <X />
+            </button>
+            <div
+              className={cn(
+                "absolute top-1/2 left-0 bottom-0 -translate-x-full -translate-y-1/2",
+                "h-72 w-16",
+                "font-montserrat font-light text-white text-lg bd:text-2xl blur-bg-bricky-brick",
+                "rounded-bl-2xl rounded-tl-2xl",
+                "inline-flex items-center justify-center",
+                "cursor-pointer"
+              )}
+              onClick={() => setOpen((prev) => !prev)}
+            >
+              <span className="block bd:-rotate-90 whitespace-nowrap pointer-events-none">{commonT("inquiry")}</span>
+            </div>
+            <ScrollableBox className="h-full">
+              <div className="px-4 bt:px-12 py-14 bt:py-16 space-y-8">
+                <h2 className="text-neutral-900 text-base bt:text-sm font-normal font-halenoir text-left bt:text-center bd:text-left leading-normal">
+                  {t.rich("description", {
+                    br: () => <br className="hidden bt:block" />,
+                  })}
+                </h2>
+                <ContactForm translations={formTranslations} />
+              </div>
+            </ScrollableBox>
+            {/* <div className="hidden bd:block col-span-1">
                   <Video
                     primaryVideoUrl={mainVideoDesktop}
                     autoPlay
@@ -185,12 +219,10 @@ export function ModalContactForm({ open, setOpen }: ModalContactFormProps) {
                     playsInline
                     className="w-full h-full object-cover"
                   />
-                </div>
-              </div>
-            </div>
+                </div> */}
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
