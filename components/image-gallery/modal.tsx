468c1d7fa1ea1207ae417ae2f3730de0690acfe4
armagansenol <armagansnl@gmail.com>
2025-05-06 18:00:59 +0300

    chore: Adjust positioning of close button in image gallery modal
    - Moved the close button's vertical position from top-4 to top-8 for improved accessibility and visual balance.

--- components/image-gallery/modal.tsx
+++ components/image-gallery/modal.tsx
@@ -19,7 +19,7 @@ export function ImageGalleryModal() {
   return (
     <Dialog defaultOpen={isOpen} open={isOpen} onOpenChange={(open) => !open && closeModal()}>
       <DialogContent className="w-screen h-screen min-h-screen min-w-screen min-w-none max-w-none p-0 box-shadow-none bg-transparent border-none flex items-center justify-center z-[var(--z-modal)]">
-        <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground z-50">
+        <DialogClose className="absolute right-4 top-8 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground z-50">
           <X className="text-white w-6 h-6" />
         </DialogClose>
         <ImageGallery slides={slides} currentSlide={currentSlide} />
