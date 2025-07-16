8343fba7dab9bb37ac9ba514f079b04fc7e6e58f
armagansenol <armagansnl@gmail.com>
2025-07-16 13:13:29 +0300

    refactor: Update citys-park layout and components for improved visual appeal
    - Introduced AnimatedLine component for enhanced visual transitions.
    - Replaced VideoWithPlayButton with VideoSection for better video integration.
    - Revised header text for clarity and engagement.
    - Adjusted typography and layout for improved readability and consistency.

--- app/[locale]/citys-park/page.tsx
+++ app/[locale]/citys-park/page.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils"
 import { useTranslations } from "next-intl"
 
+import { AnimatedLine } from "@/components/animated-line"
 import { ScaleOut } from "@/components/animations/scale-out"
 import { GsapSplitText } from "@/components/gsap-split-text"
 import { IconCitysParkLogo } from "@/components/icons"
@@ -8,12 +9,12 @@ import { LinkToPage } from "@/components/link-to-page"
 import { LogoSection } from "@/components/logo-section"
 import { MaskedParallaxImage } from "@/components/masked-parallax-image"
 import { MembersClubItem } from "@/components/members-club-item"
-import { Video } from "@/components/utility/video"
-import { VideoWithPlayButton } from "@/components/utility/video-with-play-button"
-import { Wrapper } from "@/components/wrapper"
-import { citysParkVideo, gsapGlobalClasses, pinarVeCemilAktasVideo, sections } from "@/lib/constants"
-import { breakpoints } from "@/styles/config.mjs"
 import { SectionsMenuInitializer } from "@/components/sections-menu-initializer"
+import { Video } from "@/components/utility/video"
+import { VideoSection } from "@/components/video-section"
+import { Wrapper } from "@/components/wrapper"
+import { citysParkVideo, gsapGlobalClasses, sections } from "@/lib/constants"
+import { breakpoints } from "@/styles/config.mjs"
 
 export default function Page() {
   const t = useTranslations("citys-park")
@@ -69,10 +70,10 @@ export default function Page() {
       <section className="relative z-20 bg-white py-5">
         <LogoSection foregroundLogo={<IconCitysParkLogo fill="#5D7261" />} foregroundDuration={0.5} />
         <div className="section-container py-20 flex flex-col items-center gap-20">
-          <h2 className="font-primary font-medium text-black text-2xl lg:text-2xl xl:text-3xl xl:leading-normal 2xl:text-[40px] 2xl:leading-tight xl:max-w-4xl 2xl:max-w-6xl text-center">
+          <h2 className="font-primary font-medium text-black text-2xl lg:text-2xl xl:text-3xl 2xl:text-5xl xl:leading-normal 2xl:leading-tight xl:max-w-4xl 2xl:max-w-6xl text-center">
             <GsapSplitText stagger={0.2} splitBy="lines" duration={1.5}>
-              Şehrin ortasında, sizi yavaşlatan ve merkeze alan benzersiz bir deneyim: City’s Park. City’s ile yaşam
-              yeniden tasarlandı.
+              Şehrin ortasında, sizi yavaşlatan ve <br /> merkeze alan benzersiz bir deneyim. <br /> Yaşam Yeniden
+              Tasarlandı: CITY&apos;S.
             </GsapSplitText>
           </h2>
           <div className={cn("relative w-full h-[90vh]", gsapGlobalClasses.fadeIn)}>
@@ -83,7 +84,8 @@ export default function Page() {
           </div>
         </div>
       </section>
-      <section className="relative z-20 bg-white mb-20">
+      <section className="relative z-20 bg-white">
+        <AnimatedLine direction="horizontal" />
         {items.map((item, i) => (
           <MembersClubItem
             key={i}
@@ -93,15 +95,14 @@ export default function Page() {
           />
         ))}
       </section>
-      <section className={cn("relative section-container", gsapGlobalClasses.fadeIn)}>
-        <div className="w-full h-[350px] bt:h-auto bt:aspect-video relative z-10 flex items-center justify-center bg-black">
-          <VideoWithPlayButton
-            primaryVideoUrl={pinarVeCemilAktasVideo}
-            thumbnail="/img/thumbnail-pinar-cemil-aktas.jpg"
-            title="Peyzaj: Bir Vaha Tasarımı..."
-          />
-        </div>
+      <section className={cn("relative section-container py-20", gsapGlobalClasses.fadeIn)}>
+        <VideoSection
+          primaryVideoUrl={citysParkVideo}
+          thumbnail="/img/thumbnail-pinar-cemil-aktas.jpg"
+          title="Peyzaj: Bir Vaha Tasarımı..."
+        />
       </section>
+      <AnimatedLine direction="horizontal" />
       <LinkToPage
         previous={{ title: "Daireler", href: "/residences" }}
         next={{ title: "City's Members Club", href: "/citys-members-club" }}
