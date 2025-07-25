7624c57618a8144740330badedf81fc65b9fed25
armagansenol <armagansnl@gmail.com>
2025-07-25 13:38:21 +0300

    refactor: Enhance footer layout and styling for improved responsiveness
    - Updated text sizes in the footer for better visibility on larger screens.
    - Adjusted logo and menu section widths for a more balanced layout.
    - Improved the structure of the contact and legal sections for better alignment and readability.
    - Enhanced mobile accordion functionality for menu and legal sections to improve user experience.

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -17,7 +17,7 @@ const styles = {
   // Text size patterns
   textSizes: {
     linkTextSm: "text-base lg:text-xs xl:text-sm",
-    linkText: "text-base lg:text-xs xl:text-lg",
+    linkText: "text-base lg:text-xs xl:text-lg 2xl:text-xl",
     headingText: "text-lg lg:text-sm xl:text-2xl 2xl:text-3xl text-white",
     contactText: "text-base lg:text-sm xl:text-lg",
     copyrightCredit: "text-[0.8rem] lg:text-xs xl:text-base text-white",
@@ -103,149 +103,153 @@ export function Footer() {
         <div className="flex flex-col-reverse items-stretch lg:flex-row lg:items-center gap-12 lg:gap-0">
           {/* Logo Section */}
           <div className="w-full lg:w-3/12 flex flex-col items-center justify-center gap-8 lg:gap-12 mr-auto mb-16">
-            <LocalizedLink href="/" className="w-[200px] lg:w-[200px] xl:w-[280px] 2xl:w-[300px]">
+            <LocalizedLink href="/" className="w-[200px] lg:w-[200px] xl:w-[260px] 2xl:w-[300px]">
               <Logo fill={colors["white"]} />
             </LocalizedLink>
           </div>
-          <div className="w-full lg:w-8/12 flex flex-col items-stretch lg:flex-row pb-7 lg:pb-0 relative">
-            <ScrollToTop className={cn("text-3xl 3xl:text-4xl", "absolute top-0 right-0 -translate-y-3/4 z-50")} />
-            {/* Contact Section */}
-            <div className="w-full lg:w-4/12 py-10 lg:py-0">
-              <h5 className={cn(styles.textSizes.headingText, styles.layout.sectionHeader)}>{t("contact")}</h5>
-              <div className="flex flex-col items-stretch gap-6 mr-0 xl:mr-10 pr-10">
-                <div className="space-y-3">
-                  <span className={cn("block", styles.textSizes.contactText, "text-white whitespace-pre-line")}>
-                    {t.rich("contactInfo", {
-                      br: () => <br />,
-                    })}
-                  </span>
-                  <a
-                    href="https://maps.app.goo.gl/2hSJUsgo2U198Kqq9"
-                    className={cn(
-                      "block",
-                      "text-white whitespace-pre-line",
-                      styles.textSizes.linkText,
-                      styles.interactions.opacityHover,
-                      "xl:leading-tight"
-                    )}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span className="font-bold">City’s İstanbul AVM</span> <br />
-                    <span>İçerenköy, Çayır Cd No: 1,</span>
-                    <span>34752 Ataşehir/Istanbul</span>
-                  </a>
-                  <a
-                    href="mailto:info@citysresidences.com"
-                    className={cn(
-                      "block",
-                      styles.textSizes.linkText,
-                      "text-white whitespace-pre-line",
-                      styles.interactions.opacityHover
-                    )}
-                  >
-                    info@citysresidences.com
-                  </a>
-                  <a
-                    href="tel:+902162666600"
-                    className={cn(
-                      "block",
-                      "text-white font-bold whitespace-pre-line",
-                      styles.textSizes.linkText,
-                      styles.interactions.opacityHover
-                    )}
-                  >
-                    +90 (216) 266 66 00
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* Menu Section */}
-            <div className="w-full lg:w-4/12">
-              {/* desktop */}
-              <div className={styles.layout.desktopOnly}>
-                <h5 className={cn(styles.textSizes.headingText, styles.layout.sectionHeader)}>{t("menu")}</h5>
-                <div className="flex flex-col flex-wrap gap-y-2 gap-x-6 h-48">
-                  {footerItems.menu.map((item, i) => (
-                    <LocalizedLink
-                      key={i}
-                      href={item.href}
-                      className={cn(styles.textSizes.linkText, styles.interactions.linkHover)}
-                    >
-                      {item.title}
-                    </LocalizedLink>
-                  ))}
-                </div>
-              </div>
-              {/* mobile */}
-              <Accordion className={styles.layout.mobileOnly} type="single" collapsible>
-                <AccordionItem value="item-1">
-                  <AccordionTrigger className={cn(styles.iconSizes.accordion, styles.layout.sectionHeader)}>
-                    <h5 className={cn(styles.textSizes.mobileAccordionHeading, "font-normal")}>{t("menu")}</h5>
-                  </AccordionTrigger>
-                  <AccordionContent className="py-4">
-                    <div className="flex flex-col gap-y-2 gap-x-6 mr-0 xl:mr-12">
-                      {footerItems.menu.map((item, i) => (
-                        <LocalizedLink
-                          key={i}
-                          href={item.href}
-                          className={cn(styles.textSizes.mobileAccordionLink, styles.interactions.linkHover)}
-                        >
-                          {item.title}
-                        </LocalizedLink>
-                      ))}
-                    </div>
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </div>
-            {/* Legal Section */}
-            <div className="w-full lg:w-4/12 ml-24 opacity-60">
-              {/* desktop */}
-              <div className={styles.layout.desktopOnly}>
-                <h5 className={cn(styles.textSizes.headingText, styles.layout.sectionHeader, "opacity-0")}>
-                  {t("legal")}
-                </h5>
-                <div className="space-y-2">
-                  <h5 className={cn("block", styles.textSizes.linkTextSm, styles.interactions.linkHover, "text-white")}>
-                    {t("legal")}
-                  </h5>
-                  {footerItems.legal.map((item, i) => (
-                    <LocalizedLink
+          <div className="w-full lg:w-8/12 flex flex-col items-stretch pb-7 lg:pb-0 relative">
+            <ScrollToTop className={cn("text-3xl 3xl:text-4xl ml-auto lg:w-3/12")} />
+            <div className="flex flex-col lg:flex-row">
+              {/* Contact Section */}
+              <div className="w-full lg:w-4/12 py-10 lg:py-0">
+                <h5 className={cn(styles.textSizes.headingText, styles.layout.sectionHeader)}>{t("contact")}</h5>
+                <div className="flex flex-col items-stretch gap-6 mr-0 xl:mr-10 pr-10">
+                  <div className="space-y-3">
+                    <span className={cn("block", styles.textSizes.contactText, "text-white whitespace-pre-line")}>
+                      {t.rich("contactInfo", {
+                        br: () => <br />,
+                      })}
+                    </span>
+                    <a
+                      href="https://maps.app.goo.gl/2hSJUsgo2U198Kqq9"
+                      className={cn(
+                        "block",
+                        "text-white whitespace-pre-line",
+                        styles.textSizes.linkText,
+                        styles.interactions.opacityHover,
+                        "xl:leading-tight"
+                      )}
                       target="_blank"
                       rel="noopener noreferrer"
-                      key={i}
-                      href={item.href}
-                      className={cn("block", styles.textSizes.linkTextSm, styles.interactions.linkHover)}
                     >
-                      {item.title}
-                    </LocalizedLink>
-                  ))}
+                      <span className="font-bold">City’s İstanbul AVM</span> <br />
+                      <span>İçerenköy, Çayır Cd No: 1,</span>
+                      <span>34752 Ataşehir/Istanbul</span>
+                    </a>
+                    <a
+                      href="mailto:info@citysresidences.com"
+                      className={cn(
+                        "block",
+                        styles.textSizes.linkText,
+                        "text-white whitespace-pre-line",
+                        styles.interactions.opacityHover
+                      )}
+                    >
+                      info@citysresidences.com
+                    </a>
+                    <a
+                      href="tel:+902162666600"
+                      className={cn(
+                        "block",
+                        "text-white font-bold whitespace-pre-line",
+                        styles.textSizes.linkText,
+                        styles.interactions.opacityHover
+                      )}
+                    >
+                      +90 (216) 266 66 00
+                    </a>
+                  </div>
                 </div>
               </div>
-              {/* mobile */}
-              <Accordion className={cn("w-full", styles.layout.mobileOnly)} type="single" collapsible>
-                <AccordionItem value="item-1">
-                  <AccordionTrigger className={cn(styles.iconSizes.accordion, styles.layout.sectionHeader)}>
-                    <h5 className={cn(styles.textSizes.mobileAccordionHeading, "font-normal")}>{t("legal")}</h5>
-                  </AccordionTrigger>
-                  <AccordionContent className="py-4">
-                    <div className="space-y-2">
-                      {footerItems.legal.map((item, i) => (
-                        <LocalizedLink
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          key={i}
-                          href={item.href}
-                          className={cn("block", styles.textSizes.mobileAccordionLink, styles.interactions.linkHover)}
-                        >
-                          {item.title}
-                        </LocalizedLink>
-                      ))}
-                    </div>
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
+              {/* Menu Section */}
+              <div className="w-full lg:w-4/12">
+                {/* desktop */}
+                <div className={styles.layout.desktopOnly}>
+                  <h5 className={cn(styles.textSizes.headingText, styles.layout.sectionHeader)}>{t("menu")}</h5>
+                  <div className="flex flex-col flex-wrap gap-y-2 gap-x-6 h-48">
+                    {footerItems.menu.map((item, i) => (
+                      <LocalizedLink
+                        key={i}
+                        href={item.href}
+                        className={cn(styles.textSizes.linkText, styles.interactions.linkHover)}
+                      >
+                        {item.title}
+                      </LocalizedLink>
+                    ))}
+                  </div>
+                </div>
+                {/* mobile */}
+                <Accordion className={styles.layout.mobileOnly} type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className={cn(styles.iconSizes.accordion, styles.layout.sectionHeader)}>
+                      <h5 className={cn(styles.textSizes.mobileAccordionHeading, "font-normal")}>{t("menu")}</h5>
+                    </AccordionTrigger>
+                    <AccordionContent className="py-4">
+                      <div className="flex flex-col gap-y-2 gap-x-6 mr-0 xl:mr-12">
+                        {footerItems.menu.map((item, i) => (
+                          <LocalizedLink
+                            key={i}
+                            href={item.href}
+                            className={cn(styles.textSizes.mobileAccordionLink, styles.interactions.linkHover)}
+                          >
+                            {item.title}
+                          </LocalizedLink>
+                        ))}
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </div>
+              {/* Legal Section */}
+              <div className="lg:w-3/12 ml-auto opacity-60">
+                {/* desktop */}
+                <div className={styles.layout.desktopOnly}>
+                  <h5 className={cn(styles.textSizes.headingText, styles.layout.sectionHeader, "opacity-0")}>
+                    {t("legal")}
+                  </h5>
+                  <div className="space-y-2">
+                    <h5
+                      className={cn("block", styles.textSizes.linkTextSm, styles.interactions.linkHover, "text-white")}
+                    >
+                      {t("legal")}
+                    </h5>
+                    {footerItems.legal.map((item, i) => (
+                      <LocalizedLink
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        key={i}
+                        href={item.href}
+                        className={cn("block", styles.textSizes.linkTextSm, styles.interactions.linkHover)}
+                      >
+                        {item.title}
+                      </LocalizedLink>
+                    ))}
+                  </div>
+                </div>
+                {/* mobile */}
+                <Accordion className={cn("w-full", styles.layout.mobileOnly)} type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className={cn(styles.iconSizes.accordion, styles.layout.sectionHeader)}>
+                      <h5 className={cn(styles.textSizes.mobileAccordionHeading, "font-normal")}>{t("legal")}</h5>
+                    </AccordionTrigger>
+                    <AccordionContent className="py-4">
+                      <div className="space-y-2">
+                        {footerItems.legal.map((item, i) => (
+                          <LocalizedLink
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            key={i}
+                            href={item.href}
+                            className={cn("block", styles.textSizes.mobileAccordionLink, styles.interactions.linkHover)}
+                          >
+                            {item.title}
+                          </LocalizedLink>
+                        ))}
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </div>
             </div>
           </div>
         </div>
