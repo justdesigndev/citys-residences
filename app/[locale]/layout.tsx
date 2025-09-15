bc34bf5c47e3502d7b66457205bf5e899e320966
armagansenol <armagansnl@gmail.com>
2025-09-15 10:31:55 +0300

    refactor: Integrate AlotechWidget into layout for enhanced functionality
    - Added AlotechWidget component to the layout for improved user interaction.
    - Updated the rendering of the widget to ensure it is now active in the layout structure.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -15,6 +15,7 @@ import { Preloader, PreloaderClient } from "@/components/preloader"
 import { ReactQueryProvider } from "@/components/react-query-provider"
 import { RealViewport } from "@/components/real-viewport"
 import { StickyContactMenu } from "@/components/sticky-contact-menu"
+import { AlotechWidget } from "@/components/alotech-widget"
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -215,7 +216,7 @@ export default async function LocaleLayout({
       <head>
         <StyleVariables colors={colors} themes={themes} />
         {/* <UTMWidgetInit /> */}
-        {/* <AlotechWidget /> */}
+        <AlotechWidget />
       </head>
       <body
         className={`${halenoir.variable} ${montserrat.variable} ${montaguSlab.variable} ${suisseIntl.variable} ${aktivGrotesk.variable} ${copperplate.variable} antialiased`}
