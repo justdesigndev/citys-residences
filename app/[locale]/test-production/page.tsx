89e6a8b8ee6e860d68a12d626446be64b23f9e48
armagansenol <armagansnl@gmail.com>
2025-11-02 14:51:31 +0300

    feat(test-page): create new page component for city sections and data fetching
    - Introduce TestPage component to aggregate various city-related sections including Home, ProjectSection, and ResidencesSection.
    - Implement asynchronous data fetching for CitysPark, CitysMembersClub, and CitysLiving components based on locale.
    - Structure the layout using a Wrapper component for consistent styling across sections.

---
+++ app/[locale]/test-production/page.tsx
@@ -0,0 +1,41 @@
+import { Wrapper } from '@/components/wrapper'
+
+import CitysIstanbulAvm from '@/components/sections/citys-istanbul-avm'
+import CitysLiving from '@/components/sections/citys-living'
+import CitysMembersClub from '@/components/sections/citys-members-club'
+import CitysPark from '@/components/sections/citys-park'
+import { CitysTimes } from '@/components/sections/citys-times'
+import Home from '@/components/sections/home'
+import ProjectSection from '@/components/sections/project'
+import ResidencesSection from '@/components/sections/residences'
+import {
+  fetchCitysLivingData,
+  fetchCitysMembersClubData,
+  fetchCitysParkData,
+} from '@/lib/api/queries'
+
+export default async function TestPage({
+  params,
+}: {
+  params: { locale: string }
+}) {
+  const citysParkData = await fetchCitysParkData(params.locale)
+  const citysParkDataItems = citysParkData.data || []
+  const citysMembersClubData = await fetchCitysMembersClubData(params.locale)
+  const citysMembersClubDataItems = citysMembersClubData.data || []
+  const citysLivingData = await fetchCitysLivingData(params.locale)
+  const citysLivingDataItems = citysLivingData.data || []
+
+  return (
+    <Wrapper>
+      <Home params={params} />
+      <ProjectSection params={params} />
+      <ResidencesSection params={params} />
+      <CitysPark data={citysParkDataItems} />
+      <CitysMembersClub data={citysMembersClubDataItems} />
+      <CitysLiving data={citysLivingDataItems} />
+      <CitysIstanbulAvm />
+      <CitysTimes />
+    </Wrapper>
+  )
+}
