9c24ef9937595abc5471c5a545cc7c74337ea9df
armagansenol <armagansnl@gmail.com>
2025-11-12 19:36:57 +0300

    feat: enhance page component with dynamic sections for improved layout
    - Introduce dynamic imports for CitysIstanbulAvm and CitysTimes components to optimize loading.
    - Add Home and ResidencesSection components to the page layout for a more comprehensive user experience.
    - Refactor the Page component to support asynchronous rendering, enhancing performance and responsiveness.

--- app/[locale]/test/page.tsx
+++ app/[locale]/test/page.tsx
@@ -1,10 +1,25 @@
-import ProjectSection from '@/components/sections/project'
-import { Wrapper } from '@/components/wrapper'
+import dynamic from 'next/dynamic'
 
-export default function Page({ params }: { params: { locale: string } }) {
+import { Wrapper } from '@/components/wrapper'
+import Home from '@/components/sections/home'
+import ProjectSection from '@/components/sections/project'
+import ResidencesSection from '@/components/sections/residences'
+
+const CitysIstanbulAvm = dynamic(
+  () => import('@/components/sections/citys-istanbul-avm')
+)
+const CitysTimes = dynamic(() => import('@/components/sections/citys-times'))
+
+export default async function Page({ params }: { params: { locale: string } }) {
   return (
-    <Wrapper>
-      <ProjectSection params={params} />
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Home params={params} />
+        <ProjectSection params={params} />
+        <ResidencesSection params={params} />
+        <CitysIstanbulAvm />
+        <CitysTimes locale={params.locale} />
+      </Wrapper>
+    </>
   )
 }
