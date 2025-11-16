052f0bcb83e71c7072a20cfbf72986795337f144
armagansenol <armagansnl@gmail.com>
2025-11-16 12:15:21 +0300

    refactor: comment out loading attribute in OptimizedVideo component for clarity
    - Commented out the loading attribute in the video element to enhance code readability and maintainability.

--- components/optimized-video/index.tsx
+++ components/optimized-video/index.tsx
@@ -68,7 +68,7 @@ export function OptimizedVideo({ playbackId, aspectRatio }: Props) {
         style={{
           filter: 'grayscale(20%)',
         }}
-        loading='lazy'
+        // loading='lazy'
       />
       <video
         ref={ref}
